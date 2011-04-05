e12b9286d620145c0cc2987a4ed8293025b0f0c9
Adam Gandelman <adam@janrain.com>
2011-04-05 15:06:22 -0700

    Fuck aterm, use xterm

--- bin/adjust_display.sh
+++ bin/adjust_display.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-
+[[ $1 == "home" ]] && timing="-r 60"
 xrandr | grep "VGA1 connected"
 CONNECTED=$?
+
 if [[ $CONNECTED -eq 0 ]] ; then
 	xrandr --output LVDS1 --off
 	xrandr --output VGA1 --auto
-	xrandr --output VGA1 -s 1920x1080
+	xrandr --output VGA1 -s 1920x1080 $timing
 	ratpoison -c "restart"
 	$HOME/.ratpoison/bin/launch_trayer.sh restart
 else
