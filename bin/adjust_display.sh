8b96c0d2eb760ea94924b5c5bffefc32fd9dffc4
Adam Gandelman <adam.gandelman@canonical.com>
2011-08-27 18:12:24 -0700

    Use gnome-terminal, some updates

--- bin/adjust_display.sh
+++ bin/adjust_display.sh
@@ -4,16 +4,19 @@ xrandr | grep "VGA1 connected"
 CONNECTED=$?
 
 if [[ $CONNECTED -eq 0 ]] ; then
-	xrandr --output LVDS1 --off
-	xrandr --output VGA1 --auto
-	xrandr --output VGA1 -s 1920x1080 $timing
-	ratpoison -c "restart"
-	$HOME/.ratpoison/bin/launch_trayer.sh restart
+  xrandr --output VGA1 --auto
+  sleep 1
+  xrandr --output LVDS1 --off
+  ratpoison -c "restart"
+  sleep 1
+  $HOME/.ratpoison/bin/launch_trayer.sh restart
 else
-	xrandr --output LVDS1 --auto
-	xrandr --output VGA1 --off
-	xrandr --output LVDS -s 1366x768
-	ratpoison -c "restart"
-	$HOME/.ratpoison/bin/launch_trayer.sh restart
+  xrandr --output LVDS1 --auto
+  sleep 1
+  xrandr --output VGA1 --off
+  #xrandr --output LVDS -s 1366x768
+  ratpoison -c "restart"
+  sleep 1
+  $HOME/.ratpoison/bin/launch_trayer.sh restart
 fi
 
